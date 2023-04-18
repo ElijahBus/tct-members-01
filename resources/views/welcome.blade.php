58a7c72980baf761230e1d1d3577c20b6c220af2
Elijah <elijahbus.ny@gmail.com>
2023-04-18 15:37:03 +0200

    Fix font-size on brand  title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,7 @@
 
 <nav class="navbar bg-light">
     <div class="container-fluid">
-        <span class="navbar-brand mb-0 h1">TCT</span>
+        <h1 class="navbar-brand mb-0 tct-title-primary">TCT</h1>
     </div>
 </nav>
 
